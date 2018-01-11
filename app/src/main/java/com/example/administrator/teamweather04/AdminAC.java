0e26bdc9a5fc80f45a854dde5a3684158a105236
wzf383 <383225639@qq.com>
2018-01-11 11:14:25 +0800

    管理员修改密码

--- app/src/main/java/com/example/administrator/teamweather04/AdminAC.java
+++ app/src/main/java/com/example/administrator/teamweather04/AdminAC.java
@@ -1,17 +1,53 @@
 package com.example.administrator.teamweather04;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminAC extends AppCompatActivity {
+    Button btn1;
+    Button btn2;
+    Button btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_ac);
         setTitle("管理员用户");
+
+        btn1=(Button)findViewById(R.id.btn1);
+        btn2=(Button)findViewById(R.id.btn2);
+        btn3=(Button)findViewById(R.id.btn3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(AdminAC.this,Showgradetype.class);
+                startActivity(intent);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(AdminAC.this,AdminShowMessage.class);
+                startActivity(intent);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(AdminAC.this,AdminModPwd.class);
+                startActivity(intent);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
